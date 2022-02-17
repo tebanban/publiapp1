ba7bdaa8b8ff76baecd326b1a2953b7e670c014b
tebanban <estebancalf@gmail.com>
2022-02-17 13:48:18 -0600

    search bar

--- src/front/js/pages/app.js
+++ src/front/js/pages/app.js
@@ -1,31 +1,72 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-
 import "../../styles/app.scss";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Row, Form } from "react-bootstrap";
 
 export const App = () => {
 	const { store, actions } = useContext(Context);
+	const [query, setQuery] = useState("");
 
+	const data = store.site.filter(index => {
+		if (query === "") {
+			return index;
+		} else if (index.code.toLowerCase().includes(query.toLowerCase())) {
+			return index;
+		}
+	});
+	console.log(query);
 	return (
-		<div className="container">
+		<div>
 			<div className="container">
+				<Row className="mb-1">
+					<Col md={4}>
+						<input
+							onChange={e => setQuery(e.target.value)}
+							type="text"
+							className="form-control"
+							id="inputSearch"
+							placeholder="Search"></input>
+					</Col>
+					<Col md={4}>
+						<Form.Group>
+							<select id="inputState" className="form-control">
+								<option defaultValue>Filtrar por estado...</option>
+								<option>Alquilada</option>
+								<option>Libre</option>
+								<option>Reservada</option>
+								<option>Deshabilitada</option>
+							</select>
+						</Form.Group>
+					</Col>
+					<Col md={4}>
+						<Form.Group>
+							<select id="inputState" className="form-control">
+								<option defaultValue>Filtrar por provincia...</option>
+								<option>San José</option>
+								<option>Alajuela</option>
+								<option>Heredia</option>
+								<option>Cartago</option>
+								<option>Puntarenas</option>
+								<option>Guanacaste</option>
+								<option>Limón</option>
+							</select>
+						</Form.Group>
+					</Col>
+				</Row>
 				<div className="listheader list-group-item d-flex justify-content-between">
-					<div className="col-1 ">img</div>
 					<div className="col-1"> Código </div>
 					<div className="col-3"> Nombre</div>
 					<div className="col-3"> Ubicación</div>
 					<div className="col-3"> Contacto</div>
 					<div className="col-1"> Accion</div>
 				</div>
-				{store.site.map((item, index) => {
+				{data.map((item, index) => {
 					return (
 						<div
 							key={index}
 							className="list-group-item d-flex justify-content-between"
 							style={{ background: item.background }}>
-							<div className="col-1"> img </div>
 							<div className="col-1">
 								<Link to={"/sitedetail/" + index}>
 									<span>{item.code}</span>
@@ -47,7 +88,7 @@ export const App = () => {
 			</div>
 			<br />
 			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
+				<button className="btn btn-primary">Regresar</button>
 			</Link>
 		</div>
 	);
